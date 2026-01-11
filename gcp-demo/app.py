562e0494c5e1a1f3dbb015f024bff0028316b49e
Trần Vĩnh Phát <tranvinhphat310305@gmail.com>
2026-01-11 15:49:23 +0700

    Update app.py

--- gcp-demo/app.py
+++ gcp-demo/app.py
@@ -1,31 +1,13 @@
 from flask import Flask
-import pymysql
-import os
 
 app = Flask(__name__)
 
-def get_db_connection():
-    return pymysql.connect(
-        host=os.environ["DB_HOST"],
-        port=int(os.environ.get("DB_PORT", 3306)),
-        user=os.environ["DB_USER"],
-        password=os.environ["DB_PASS"],
-        database=os.environ["DB_NAME"],
-        connect_timeout=5
-    )
-
 @app.route("/")
-def index():
-    try:
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute("SELECT name FROM users")
-        users = cursor.fetchall()
-        conn.close()
-        return f"<pre>{users}</pre>"
-    except Exception as e:
-        return f"<pre>{e}</pre>"
+def hello():
+    return "Hello from Google Cloud Run!"
 
 if __name__ == "__main__":
+    # Cloud Run sẽ tự set PORT
+    import os
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
